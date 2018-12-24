7620f80d013c11c1f3608634ea43ea54652b8980
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-24 15:58:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,8 +109,12 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     programmer_hash = {:alan_kay => {
-      :known_for => alans_new_info}}
+     programmer_hash = {
+       :alan_kay => {
+      :known_for => alans_new_info
+         
+       }
+     }
      
 end
 
@@ -130,5 +134,13 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+
+programmer_hash = {
+  :dennis_ritchie => {
+    :languages => "Assembly"
+}
+}
+
+
 
 end
